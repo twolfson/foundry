86afd08392bd980de5c882010ae1e7488663f48d
Todd Wolfson <todd@twolfson.com>
2013-12-07 05:13:56 -0800

    Completed assertion for git tagging

--- test/foundry_test.js
+++ test/foundry_test.js
@@ -1,8 +1,11 @@
 // Load in dependencies
 var childProcess = require('child_process');
 var path = require('path');
-var wrench = require('wrench');
 var expect = require('chai').expect;
+// TODO: Move to enclosed file system with no fear of publishing anything
+// TODO: How will this behave with gitter calls? Should we move off of git CLI? They might have customizations.
+var sinon = require('sinon');
+var wrench = require('wrench');
 var Foundry = require('../bin/foundry');
 
 // Stop exec calls from happening
@@ -56,6 +59,14 @@ describe('A release', function () {
         done(err);
       });
     });
+    before(function stubExec () {
+      this.stub = sinon.stub(shell, 'exec', function () {
+        return {};
+      });
+    });
+    after(function () {
+      this.stub.restore();
+    });
     before(function release (done) {
       var program = new Foundry();
       program.parse(['node', '/usr/bin/foundry', 'release', '0.1.0']);
@@ -63,14 +74,18 @@ describe('A release', function () {
       setTimeout(done, 1000);
     });
 
-    it('adds a git tag', function (done) {
-      process.chdir(this.gitDir);
-      childProcess.exec('git tag', function (err, stdout, stderr) {
-        if (err) {
-          return done(err);
-        }
-        expect(stdout).to.equal('0.1.0');
-      });
+    it('adds a git tag', function () {
+      expect(this.stub.args[0]).to.deep.equal(['git commit -a -m "Release 0.1.0"']);
+      expect(this.stub.args[1]).to.deep.equal(['git tag 0.1.0 -a -m "Release 0.1.0"']);
+      expect(this.stub.args[2]).to.deep.equal(['git push']);
+      expect(this.stub.args[3]).to.deep.equal(['git push --tags']);
+
+      // childProcess.exec('git tag', function (err, stdout, stderr) {
+      //   if (err) {
+      //     return done(err);
+      //   }
+      //   expect(stdout).to.equal('0.1.0');
+      // });
     });
   });
 });
