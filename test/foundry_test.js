144ca1dc47a82780e175d42abcf6c81673f9f395
Todd Wolfson <todd@twolfson.com>
2013-12-07 03:57:58 -0800

    Setting up fixture directories

--- test/foundry_test.js
+++ test/foundry_test.js
@@ -1,6 +1,7 @@
-var foundry = require('../');
+var foundry = require('../bin/foundry');
 
 // Stop exec calls from happening
+// TODO: This will become mock
 var shell = require('shelljs');
 shell.exec = function () {};
 
@@ -8,12 +9,32 @@ shell.exec = function () {};
 // DEV: WE CANNOT STOP .exec CALLS FROM OCCURRING IN ANOTHER PROCESS
 // TODO: Strongly consider running tests within a Vagrant to prevent publication since nothing is configured
 
-describe('foundry', function () {
-  before(function () {
+var tmp = shell.tempdir();
+var fixtureDir = path.join(tmp, 'foundry_test');
+before(function deleteFixtureDir (done) {
+  rmdirRecursive(fixtureDir, done);
+});
+before(function createFixtureDir (done) {
+  mkdirRecursive(fixtureDir, done);
+});
+before(function goToFixtureDir (done) {
+  process.chdir(fixtureDir);
+});
+
+function fixtureDir(name) {
+  before(function copyFixtures (done) {
 
   });
+  // TODO: Perform this
+  // before(function moveDotgitToGit (done) {
 
-  it('', function () {
+  // });
+}
 
+describe('A release', function () {
+  describe('in a git folder', function () {
+    it('adds a git tag', function () {
+
+    });
   });
 });
