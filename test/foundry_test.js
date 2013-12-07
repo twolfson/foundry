b058537799afb6003971562a2ffeb40c839d08cf
Todd Wolfson <todd@twolfson.com>
2013-12-07 04:11:32 -0800

    Filling out git-init piece of test

--- test/foundry_test.js
+++ test/foundry_test.js
@@ -1,3 +1,7 @@
+// Load in dependencies
+var childProcess = require('child_process');
+var path = require('path');
+var wrench = require('wrench');
 var foundry = require('../bin/foundry');
 
 // Stop exec calls from happening
@@ -12,15 +16,16 @@ shell.exec = function () {};
 var tmp = shell.tempdir();
 var fixtureDir = path.join(tmp, 'foundry_test');
 before(function deleteFixtureDir (done) {
-  rmdirRecursive(fixtureDir, done);
+  wrench.rmdirRecursive(fixtureDir, done);
 });
 before(function createFixtureDir (done) {
-  mkdirRecursive(fixtureDir, done);
+  wrench.mkdirRecursive(fixtureDir, done);
 });
 before(function goToFixtureDir (done) {
   process.chdir(fixtureDir);
 });
 
+// TODO: Use this... similar to that of sexy-bash-prompt
 function fixtureDir(name) {
   before(function copyFixtures (done) {
 
@@ -33,6 +38,20 @@ function fixtureDir(name) {
 
 describe('A release', function () {
   describe('in a git folder', function () {
+    before(function createGitFolder (done) {
+      this.gitDir = path.join(fixtureDir, 'git_test');
+      wrench.mkdirRecursive(this.gitDir, done);
+    });
+    before(function initializeGitFolder (done) {
+      var that = this;
+      process.chdir(this.gitDir);
+      console.log(this.gitDir);
+      // childProcess.exec('git init', function (err, stdout, stderr) {
+      //   that.stdout = stdout;
+        done(err);
+      // });
+    });
+
     it('adds a git tag', function () {
 
     });
