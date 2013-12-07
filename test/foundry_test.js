721c73eb9c6a857d2796ddcd6a4d209910ad555b
Todd Wolfson <todd@twolfson.com>
2013-12-07 03:44:25 -0800

    Taking notes for self

--- test/foundry_test.js
+++ test/foundry_test.js
@@ -1,5 +1,13 @@
 var foundry = require('../');
 
+// Stop exec calls from happening
+var shell = require('shelljs');
+shell.exec = function () {};
+
+// DEV: NEVER EVER RUN FOUNDRY VIA .exec
+// DEV: WE CANNOT STOP .exec CALLS FROM OCCURRING IN ANOTHER PROCESS
+// TODO: Strongly consider running tests within a Vagrant to prevent publication since nothing is configured
+
 describe('foundry', function () {
   before(function () {
 
