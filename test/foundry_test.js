2d73246c3ec39fa57e09713684eca9f6a59b29de
Todd Wolfson <todd@twolfson.com>
2013-12-09 02:42:52 -0800

    Adjusting timeout

--- test/foundry_test.js
+++ test/foundry_test.js
@@ -84,7 +84,7 @@ describe('A release', function () {
       program.parse(['node', '/usr/bin/foundry', 'release', '0.1.0']);
       // TODO: Figure out how to hook in better (program.parse does not provide a callback hook)
       // TODO: Maybe an EventEmitter? (error, end)
-      setTimeout(done, 1000);
+      setTimeout(done, 100);
     });
 
     it('adds a git tag', function () {
