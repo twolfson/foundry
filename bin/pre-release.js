a01b78d42775f38961e16daaf226afcbd0636bb6
Todd Wolfson <todd@twolfson.com>
2013-12-11 00:34:47 -0800

    Removed build step from pre-release.js

--- bin/pre-release.js
+++ bin/pre-release.js
@@ -5,13 +5,6 @@ module.exports = function (version, cb) {
     // TODO: Use shellquote
     // TODO: Avoid exec altogether
     shell.exec('node -e "f = \'./package.json\'; p = require(f); p.version = \'' + version + '\'; require(\'fs\').writeFileSync(f, JSON.stringify(p, null, 2));"');
-
-    // If there is a build script, run it
-    // TODO: This should be an opt-in action (not everyone will share this opinion)
-    var result = shell.exec('node -e "f = \'./package.json\'; p = require(f); process.exit(+(!(p.scripts || {}).build))"');
-    if (result.code === 0) {
-      shell.exec('npm run build');
-    }
   }
 
   // bower
