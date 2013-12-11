d000d5077f7491733a0e907d66280555126a6188
Todd Wolfson <todd@twolfson.com>
2013-12-11 02:34:38 -0800

    Ditching bulky/confusing allowDuring for event driven ban/unban

--- test/utils/child-process.js
+++ test/utils/child-process.js
@@ -42,35 +42,6 @@ exports.shellExec = {
       this._ban();
       cb(err);
     });
-  },
-  allowDuring: function (obj, key) {
-    // Outside of fn/before running (banned) ->
-    // Inside of fn (allowed) ->
-    // Outside of fn/inside callback (banned)
-    var origFn = obj[key];
-    obj[key] = function newFn (/* args, ..., cb*/) {
-      // Save references to args and that
-      var args = [].slice.call(arguments);
-      var that = this;
-
-      // TODO: Need to enable childProcess.exec as well
-      // Enable .exec until callback is run
-      var cb = args.pop();
-      exports.shellExec._allow();
-      args.push(function boundCallback (/* args */) {
-        // Re-ban before the callback
-        exports.shellExec._ban();
-
-        // Restore the original function
-        obj[key] = origFn;
-
-        // Apply the original callback
-        cb.apply(this, arguments);
-      });
-
-      // Run the original function
-      return origFn.apply(this, args);
-    };
   }
 };
 
@@ -87,35 +58,5 @@ exports.childExec = {
       this._ban();
       cb(err);
     });
-  },
-  allowDuring: function (obj, key) {
-    // Outside of fn/before running (banned) ->
-    // Inside of fn (allowed) ->
-    // Outside of fn/inside callback (banned)
-    var origFn = obj[key];
-    obj[key] = function newFn (/* args, ..., cb*/) {
-      // Save references to args and that
-      var args = [].slice.call(arguments);
-      var that = this;
-
-      // TODO: Need to enable childProcess.exec as well
-      // Enable .exec until callback is run
-      var cb = args.pop();
-      exports.childExec._allow();
-      args.push(function boundCallback (/* args */) {
-        // Re-ban before the callback
-        exports.childExec._ban();
-
-        // Restore the original function
-        obj[key] = origFn;
-
-        // Apply the original callback
-        cb.apply(this, arguments);
-      });
-
-      // Run the original function
-      return origFn.apply(this, args);
-    };
   }
-
 };
