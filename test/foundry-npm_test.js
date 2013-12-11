760df500f6ef3d505f3c4a77ee00952be09a466e
Todd Wolfson <todd@twolfson.com>
2013-12-11 01:09:26 -0800

    Moving to foundry run in npm test

--- test/foundry-npm_test.js
+++ test/foundry-npm_test.js
@@ -10,6 +10,14 @@ var childUtils = require('./utils/child-process');
 describe('A release', function () {
   describe.only('in a node module (npm)', function () {
     fixtureUtils.fixtureDir('npm');
+    before(function release (done) {
+      var program = new Foundry();
+      program.parse(['node', '/usr/bin/foundry', 'release', '0.1.0']);
+      // TODO: Figure out how to hook in better (program.parse does not provide a callback hook)
+      // TODO: Maybe an EventEmitter? (error, end)
+      setTimeout(done, 100);
+    });
+
 
     it('', function () {
       console.log(process.cwd());
