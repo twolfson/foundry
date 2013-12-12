15f171ed6f62346eae2c68a39022b622149dd5ce
Todd Wolfson <todd@twolfson.com>
2013-12-12 00:10:10 -0800

    Asserting PyPI version

--- test/foundry-pypi_test.js
+++ test/foundry-pypi_test.js
@@ -1,6 +1,8 @@
 // Load in dependencies
 var fs = require('fs');
 var expect = require('chai').expect;
+var sinon = require('sinon');
+var shell = require('shelljs');
 var fixtureUtils = require('./utils/fixtures');
 var foundryUtils = require('./utils/foundry');
 
@@ -18,14 +20,23 @@ describe('A release', function () {
         allowGitTag: true
       });
 
+      // Monitor shell.exec calls
+      var that = this;
+      program.once('postRelease#before', function () {
+        // TODO: We should be testing against shell.exec on `private: true` for the first call. No stubbing.
+        that.execStub = sinon.stub(shell, 'exec', function () {
+          return {code: 0};
+        });
+      });
+
       // Run through the release
       program.once('postRelease#after', done);
       program.parse(['node', '/usr/bin/foundry', 'release', '0.1.0']);
     });
 
-    it.skip('updates the setup.py', function () {
-      var pkgPython = fs.readFileSync(fixtureDir + '/setup.py');
-      // expect(JSON.parse(pkgPython)).to.have.property('version', '0.1.0');
+    it('updates the setup.py', function () {
+      var pkgPython = fs.readFileSync(fixtureDir + '/setup.py', 'utf8');
+      expect(pkgPython).to.contain('version=\'0.1.0\'');
     });
   });
 });
