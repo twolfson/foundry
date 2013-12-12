6297944b68c876c0d14bd75bcb3dec07d70a2aa7
Todd Wolfson <todd@twolfson.com>
2013-12-12 00:16:58 -0800

    Added test for already registered PyPI packages

--- test/foundry-pypi_test.js
+++ test/foundry-pypi_test.js
@@ -11,7 +11,8 @@ var childUtils = require('./utils/child-process');
 
 // Define our test
 describe('A release', function () {
-  describe.only('in a PyPI package', function () {
+  // TODO: Test private
+  describe('in a new PyPI package', function () {
     var fixtureDir = fixtureUtils.fixtureDir('pypi');
     before(function release (done) {
       // Introduce custom stubbing
@@ -33,6 +34,10 @@ describe('A release', function () {
       program.once('postRelease#after', done);
       program.parse(['node', '/usr/bin/foundry', 'release', '0.1.0']);
     });
+    after(function unstub () {
+      this.execStub.restore();
+    });
+
 
     it('updates the setup.py', function () {
       var pkgPython = fs.readFileSync(fixtureDir + '/setup.py', 'utf8');
@@ -45,6 +50,42 @@ describe('A release', function () {
 
     it('publishes the package', function () {
       expect(this.execStub.args[1][0]).to.contain(['python setup.py sdist']);
+    });
+  });
+
+  describe.only('in a registered PyPI package', function () {
+    var fixtureDir = fixtureUtils.fixtureDir('pypi-registered');
+    before(function release (done) {
+      // Introduce custom stubbing
+      var program = foundryUtils.create({
+        allowPreRelease: true,
+        allowGitTag: true
+      });
+
+      // Monitor shell.exec calls
+      var that = this;
+      program.once('postRelease#before', function () {
+        // TODO: We should be testing against shell.exec on `private: true` for the first call. No stubbing.
+        that.execStub = sinon.stub(shell, 'exec', function () {
+          return {code: 0};
+        });
+      });
+
+      // Run through the release
+      program.once('postRelease#after', done);
+      program.parse(['node', '/usr/bin/foundry', 'release', '0.3.0']);
+    });
+    after(function unstub () {
+      this.execStub.restore();
+    });
+
+    it('updates the setup.py', function () {
+      var pkgPython = fs.readFileSync(fixtureDir + '/setup.py', 'utf8');
+      expect(pkgPython).to.contain('version=\'0.3.0\'');
+    });
+
+    it('does not register the package', function () {
+      expect(this.execStub.args[0][0]).to.not.contain('register');
     });
   });
 });
