0e27a93c634ecde180fe3b1f2d70ec44cd3b280b
Todd Wolfson <todd@twolfson.com>
2013-12-13 00:51:56 -0800

    Iterating on foundry info

--- bin/completion/foundry-completion.bash
+++ bin/completion/foundry-completion.bash
@@ -3,8 +3,15 @@
 # https://github.com/isaacs/npm/blob/v1.3.17/lib/utils/completion.sh
 
 _foundry_completion () {
-  echo 'hai'
-  COMPREPLY=('release')
+  # COMP_WORDBREAKS=${COMP_WORDBREAKS/=/}
+  # COMP_WORDBREAKS=${COMP_WORDBREAKS/@/}
+  #   IFS=$'\n' COMPREPLY=($(COMP_CWORD="$COMP_CWORD" \
+  #                          COMP_LINE="$COMP_LINE" \
+  #                          COMP_POINT="$COMP_POINT" \
+  echo "$COMP_CWORD"
+  echo "$COMP_LINE"
+  echo "$COMP_POINT"
+  COMPREPLY=('release\n')
 }
 
 complete -F _foundry_completion foundry
