4747e1cbe6352a82f068f6f4c0b86c73553b650b
Todd Wolfson <todd@twolfson.com>
2013-12-13 00:48:48 -0800

    Release release

--- bin/completion/foundry-completion.bash
+++ bin/completion/foundry-completion.bash
@@ -1,2 +1,10 @@
 # Points of reference
 # https://github.com/git/git/blob/v1.8.5/contrib/completion/git-completion.bash
+# https://github.com/isaacs/npm/blob/v1.3.17/lib/utils/completion.sh
+
+_foundry_completion () {
+  echo 'hai'
+  COMPREPLY=('release')
+}
+
+complete -F _foundry_completion foundry
