b3f8caf76039b884242184efd6eb7c3fd30b7862
Todd Wolfson <todd@twolfson.com>
2013-12-13 01:40:50 -0800

    Finished up basic test outline

--- test/foundry-completion_test.js
+++ test/foundry-completion_test.js
@@ -3,8 +3,9 @@ var expect = require('chai').expect;
 var Foundry = require('../');
 
 describe.only('A partial `release` command', function () {
-  // TODO: It would be nice to make the split functionality into a node module
   before(function () {
+    // TODO: It would be nice to make the split functionality into a node module
+    // TODO: We have done some work on this inside of sublime-plugin-tests
     // foundry rel|e
     this.params = {
       wordIndex: 1,
@@ -14,6 +15,15 @@ describe.only('A partial `release` command', function () {
   });
 
   describe('when completed', function () {
+    before(function (done) {
+      // Create a new program and run auto-complete on it
+      var program = new Foundry();
+      var that = this;
+      program.completion(this.params, function (err, data) {
+        that.data = data;
+        done(err);
+      });
+    });
 
     it('receives `release` as an auto-complete option', function () {
 
