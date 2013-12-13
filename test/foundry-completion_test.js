eecdf8f706d6636716b353a3cf54f947719721b2
Todd Wolfson <todd@twolfson.com>
2013-12-13 01:36:34 -0800

    Continuing completion test

--- test/foundry-completion_test.js
+++ test/foundry-completion_test.js
@@ -1,3 +1,22 @@
 // Load in dependencies
 var expect = require('chai').expect;
 var Foundry = require('../');
+
+describe.only('A partial `release` command', function () {
+  // TODO: It would be nice to make the split functionality into a node module
+  before(function () {
+    // foundry rel|e
+    this.params = {
+      wordIndex: 1,
+      line: 'foundry rele',
+      linePosition: 11
+    };
+  });
+
+  describe('when completed', function () {
+
+    it('receives `release` as an auto-complete option', function () {
+
+    });
+  });
+});
