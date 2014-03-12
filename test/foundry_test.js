e921912b005418d06541778e65c63c256abf8169
Todd Wolfson <todd@twolfson.com>
2014-03-12 00:45:21 -0700

    Introducing new test suite

---
+++ test/foundry_test.js
@@ -0,0 +1,35 @@
+describe('foundry', function () {
+  describe('releasing a new package', function () {
+    it('updates the package version', function () {
+
+    });
+
+    it('commits the updates', function () {
+
+    });
+
+    it('registers the package', function () {
+
+    });
+
+    it('publishes the package', function () {
+
+    });
+  });
+
+  describe('releasing an existing package', function () {
+    it('updates the package version', function () {
+
+    });
+
+    it('does not register the package', function () {
+
+    });
+  });
+
+  describe('releasing a package with no commands', function () {
+    it('does not error out', function () {
+
+    });
+  });
+});
