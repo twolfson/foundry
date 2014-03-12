b52f89fecf14325563d5048c7685a2c8cbc97195
Todd Wolfson <todd@twolfson.com>
2014-03-12 00:48:05 -0700

    Preventing accidental executions via fixtures

--- test/foundry_test.js
+++ test/foundry_test.js
@@ -1,3 +1,17 @@
+// Load in dependencies
+var childProcess = require('child_process');
+var expect = require('chai').expect;
+var foundry = require('../');
+
+// Stop childProcess exec and spawn calls too unless people opt in to our methods
+// DEV: This is borrowed from https://github.com/twolfson/foundry/blob/0.15.0/test/utils/child-process.js
+childProcess.spawn = function () {
+  throw new Error('`childProcess.spawn` was being called with ' + JSON.stringify(arguments));
+};
+childProcess.exec = function () {
+  throw new Error('`childProcess.exec` was being called with ' + JSON.stringify(arguments));
+};
+
 describe('foundry', function () {
   describe('releasing a new package', function () {
     it('updates the package version', function () {
