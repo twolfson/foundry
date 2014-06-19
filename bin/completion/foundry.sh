debbfe135a324a0374fabc53f6c3745a9b887d3f
Todd Wolfson <todd@twolfson.com>
2014-06-19 01:42:43 -0700

    Added back discrepencies from npm

--- bin/completion/foundry.sh
+++ bin/completion/foundry.sh
@@ -1,4 +1,5 @@
 # Forked from https://github.com/isaacs/npm/blob/v1.3.17/lib/utils/completion.sh
+#!/bin/bash
 ###-begin-foundry-completion-###
 #
 # foundry command completion script
@@ -11,20 +12,29 @@ COMP_WORDBREAKS=${COMP_WORDBREAKS/=/}
 COMP_WORDBREAKS=${COMP_WORDBREAKS/@/}
 export COMP_WORDBREAKS
 
-if complete &>/dev/null; then
+if type complete &>/dev/null; then
   _foundry_completion () {
     local si="$IFS"
-
     IFS=$'\n' COMPREPLY=($(COMP_CWORD="$COMP_CWORD" \
                            COMP_LINE="$COMP_LINE" \
                            COMP_POINT="$COMP_POINT" \
                            foundry completion -- "${COMP_WORDS[@]}" \
-                           # 2>/dev/null)) || return $?
-                           )) || return $?
+                           2>/dev/null)) || return $?
     IFS="$si"
   }
   complete -F _foundry_completion foundry
-elif compctl &>/dev/null; then
+elif type compdef &>/dev/null; then
+  _foundry_completion() {
+    si=$IFS
+    compadd -- $(COMP_CWORD=$((CURRENT-1)) \
+                 COMP_LINE=$BUFFER \
+                 COMP_POINT=0 \
+                 foundry completion -- "${words[@]}" \
+                 2>/dev/null)
+    IFS=$si
+  }
+  compdef _foundry_completion foundry
+elif type compctl &>/dev/null; then
   _foundry_completion () {
     local cword line point words si
     read -Ac words
