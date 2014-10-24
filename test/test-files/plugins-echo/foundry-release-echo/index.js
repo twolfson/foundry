d2346bb4953942721130225e4dad2a685a2465c8
Todd Wolfson <todd@twolfson.com>
2014-10-23 22:52:50 -0700

    Making echo test more robust

--- test/test-files/plugins-echo/foundry-release-echo/index.js
+++ test/test-files/plugins-echo/foundry-release-echo/index.js
@@ -1,7 +1,22 @@
 // Export up to date plugin with echo upon publish
 exports.specVersion = '1.1.0';
 
+exports.updateFiles = function (params, cb) {
+  console.log('updateFiles occurred');
+  cb();
+};
+
+exports.commit = function (params, cb) {
+  console.log('commit occurred');
+  cb();
+};
+
+exports.register = function (params, cb) {
+  console.log('register occurred');
+  cb();
+};
+
 exports.publish = function (params, cb) {
-  console.log('Hello World!');
+  console.log('Publish occurred');
   cb();
 };
