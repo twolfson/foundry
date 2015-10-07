51c6eeb0b09a23f69e83e7b478e8bb8d169fd694
Todd Wolfson <todd@twolfson.com>
2015-10-07 00:33:10 -0500

    Fixed up script due to fallacy by existing global foundry

--- docs/getting-started.sh
+++ docs/getting-started.sh
@@ -30,16 +30,23 @@ EOF
 npm install twolfson/foundry-release-git#ec8aa239
 
 # Copy over symlink to our foundry instance
-ln -s ../../../bin/foundry ./node_modules/.bin/
+ln -s ../../../../bin/foundry ./node_modules/.bin/
 
 # Run our release
 #   Prepending `./node_modules/.bin/` to `PATH` can be avoided by using `foundry-cli`
 #   https://github.com/twolfson/foundry-cli
 #   or by using `npm-run-script`
 #   https://www.npmjs.org/doc/misc/npm-scripts.html#environment
-PATH="$PATH:./node_modules/.bin/"
+PATH="$PATH:$PWD/node_modules/.bin/"
 foundry release 1.0.0
-# [master c6ce921] Release 1.0.0
+# Configuring steps with FOUNDRY_VERSION: 1.0.0
+# Configuring steps with FOUNDRY_MESSAGE: Release 1.0.0
+# Running step: foundry-release-git update-files "$FOUNDRY_VERSION" "$FOUNDRY_MESSAGE"
+# Running step: foundry-release-git commit "$FOUNDRY_VERSION" "$FOUNDRY_MESSAGE"
+# [master ec7a32d] Release 1.0.0
+# Running step: foundry-release-git register "$FOUNDRY_VERSION" "$FOUNDRY_MESSAGE"
+# Running step: foundry-release-git publish "$FOUNDRY_VERSION" "$FOUNDRY_MESSAGE"
+# Pushes to remote server
 
 # See the release commit and tag
 git log --decorate --oneline
