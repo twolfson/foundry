f29409cd7b1fb769015dd1f64bb0470c92ac29da
Todd Wolfson <todd@twolfson.com>
2015-10-07 00:19:30 -0500

    Making sure our scripts work

--- docs/getting-started.sh
+++ docs/getting-started.sh
@@ -1,3 +1,13 @@
+#!/usr/bin/env bash
+# Exit on our first error
+set -e
+
+# Install a `git` foundry-release command
+npm install foundry-release-git
+
+# Copy over symlink to our foundry instance
+ln -s ../../bin/foundry ./node_modules/.bin/
+
 # Create git repo
 mkdir foundry-example
 cd foundry-example
@@ -18,7 +28,12 @@ cat > package.json <<EOF
 EOF
 
 # Run our release
-foundry release
+#   Prepending `./node_modules/.bin/` to `PATH` can be avoided by using `foundry-cli`
+#   https://github.com/twolfson/foundry-cli
+#   or by using `npm-run-script`
+#   https://www.npmjs.org/doc/misc/npm-scripts.html#environment
+PATH="$PATH:./node_modules/.bin/foundry"
+foundry release 1.0.0
 # [master c6ce921] Release 1.0.0
 
 # See the release commit and tag
